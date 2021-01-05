ebee6b50d12f8adaf47e6bee1c34bd2aa2563ce7
tuimac <tuimac.devadm01@gmail.com>
2021-01-06 00:02:05 +0900

    Create lambda function scripts

--- aws/createLambdaFunction.py
+++ aws/createLambdaFunction.py
@@ -3,26 +3,49 @@
 import boto3
 import zipfile
 import os
+import sys
 import traceback
 
 def zipSrcDir(path, filename):
     try:
-        zipf = zipfile.ZipFile(filename, 'w', zipfile.ZIP_DEFLATED)
-        for root, dirs, files in os.walk(path):
+        currentdir = os.path.dirname(os.path.realpath(__file__))
+        os.chdir(path)
+        zipf = zipfile.ZipFile(currentdir + '/' + filename, 'w', zipfile.ZIP_DEFLATED)
+        for root, dirs, files in os.walk('.'):
             for file in files:
                 zipf.write(os.path.join(root, file))
         zipf.close()
+        os.chdir(currentdir)
+    except Exception as e:
+        raise e
+
+def createFunction(client, iamRole, zipFile):
+    try:
+        response = client.create_function(
+            FunctionName = 'booksearch',
+            Runtime = 'python3.8',
+            Role = iamRole,
+            Handler = 'lambda_handler',
+            Code = { 'ZipFile': open(zipFile, 'rb').read() }
+        )
+        print(response['FunctionArn'])
     except Exception as e:
         raise e
 
 if __name__ == '__main__':
-    srcdir = '../src/backend/book_get'
+    srcdir = '../src/backend/book_get/'
     zipFileName = 'src.zip'
+
+    if len(sys.argv) != 2:
+        print('usage: ./createLambdaFunction.py <IAM role ARN>')
+        sys.exit(1)
+    iamRoleArn = sys.argv[1]
+
     try:
         client = boto3.client('lambda')
         zipSrcDir(srcdir, zipFileName)
-
-
+        createFunction(client, iamRoleArn, zipFileName)
     except:
         traceback.print_exc()
-
+    finally:
+        os.remove(zipFileName)
